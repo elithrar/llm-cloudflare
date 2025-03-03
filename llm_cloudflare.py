335b4d281335f78ef8e81abcad3a97ab6a102ceb
Matt Silverlock <matt@eatsleeprepeat.net>
2025-03-03 16:09:37 -0600

    Update llm_cloudflare.py

--- llm_cloudflare.py
+++ llm_cloudflare.py
@@ -5,8 +5,7 @@ from openai import OpenAI, APIConnectionError, RateLimitError, APIStatusError
 from pydantic import Field
 from typing import Optional, List
 
-DEFAULT_MODEL = "@cf/meta/llama-3.1-8b-instruct"
-
+DEFAULT_MODEL = "@cf/meta/llama-3.3-70b-instruct-fp8-fast"
 
 @llm.hookimpl
 def register_models(register):
